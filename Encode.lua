6cb2e3a317bb8046a34c0e55c8bc6623050e43d0
Emil <50961259+EmK530@users.noreply.github.com>
2025-05-02 05:13:53 +0200

    New datatype support

--- Encode.lua
+++ Encode.lua
@@ -91,6 +91,12 @@ local function OutlineMoment(v)
 	return buf
 end
 
+local mf = math.floor
+local mc = math.clamp
+local function toByte(num)
+	return mc(mf(num*255),0,255)
+end
+
 local functions
 functions = {
 	function(v) -- String
@@ -140,14 +146,43 @@ functions = {
 	function(v) -- CFrameEuler
 		return OutlineMoment(v)
 	end,
-	function(v) -- Color3
-		return OutlineMoment(v)
+	function(v , ident) -- Color3
+		local buf
+		local func
+		local off
+		local dat = v.Data
+		if v.comp then
+			buf = bucr(#dat*4+2)
+			func = buwf32
+			off = 4
+		else
+			buf = bucr(#dat*8+2)
+			func = buwf64
+			off = 8
+		end
+		local o = 1
+		if ident == false then
+			o = 0
+		else
+			buwu8(buf,0,(if v.Brick then 1 else 0))
+		end
+		buwu8(buf,o,(if v.comp then 1 else 0))
+		for i,d in pairs(dat) do
+			func(buf,(i-1)*off+(o+1),d)
+		end
+		return buf
 	end,
-	function(v) -- Color3b
-		local buf = bucr(3)
-		buwu8(buf,0,v.R)
-		buwu8(buf,1,v.G)
-		buwu8(buf,2,v.B)
+	function(v, ident) -- Color3b
+		local buf = bucr(4)
+		local o = 1
+		if ident == false then
+			o = 0
+		else
+			buwu8(buf,0,(if v.Brick then 1 else 0))
+		end
+		buwu8(buf,o,v.R)
+		buwu8(buf,o+1,v.G)
+		buwu8(buf,o+2,v.B)
 		return buf
 	end,
 	function(v) -- Table
@@ -168,7 +203,68 @@ functions = {
 		return out
 	end,
 	nil, -- DO NOT USE: End marker for tables.
-	nil -- not populated
+	nil, -- DO NOT USE: Handled elsewhere, begin marker for dictionaries.
+	function(v) -- nil
+		return bucr(0)
+	end,
+	function(v) -- ColorSequence
+		local func
+		local off
+		local dat = v.Data
+		local kp = v.Value.Keypoints
+		local count = #kp
+		if v.comp1 then
+			func = buwf32
+			off = 4
+		else
+			func = buwf64
+			off = 8
+		end
+		local sz = (if v.comp2 then 3 else off*3)*count+2
+		local varlen = module.EncodeVarLength(count)
+		local lensz = bule(varlen)
+		local buf = bucr(count*off+sz+lensz)
+		buco(buf,0,varlen,0,lensz)
+		buwu8(buf,lensz,(if v.comp1 then 1 else 0))
+		buwu8(buf,lensz+1,(if v.comp2 then 1 else 0))
+		local pos = lensz+2
+		for _,k in kp do
+			func(buf,pos,k.Time)
+			pos += off
+		end
+		for _,k in kp do
+			local c = k.Value
+			if v.comp2 then
+				buwu8(buf,pos,toByte(c.R))
+				buwu8(buf,pos+1,toByte(c.G))
+				buwu8(buf,pos+2,toByte(c.B))
+				pos += 3
+			else
+				-- looks dumb but should technically be faster
+				func(buf,pos,c.R)
+				pos += off
+				func(buf,pos,c.G)
+				pos += off
+				func(buf,pos,c.B)
+				pos += off
+				print(pos)
+			end
+		end
+		return buf
+	end,
+	function(v) -- Vector2int16
+		local buf = bucr(4)
+		buwu16(buf,0,v.Data[1]+32768)
+		buwu16(buf,2,v.Data[2]+32768)
+		return buf
+	end,
+	function(v) -- Vector3int16
+		local buf = bucr(6)
+		buwu16(buf,0,v.Data[1]+32768)
+		buwu16(buf,2,v.Data[2]+32768)
+		buwu16(buf,4,v.Data[3]+32768)
+		return buf
+	end,
 }
 
 module.Convert = function(v)
